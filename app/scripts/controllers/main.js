ae49beef1fbb7823534f088830c306d9103b69c6
Gerardo Castro <gerardo_ct@hotmail.com>
2016-07-20 15:16:13 -0500

    init

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yoteprestoclientApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the yoteprestoclientApp
+ */
+angular.module('yoteprestoclientApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
